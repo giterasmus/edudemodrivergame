e87b2ce8b049ba88c5b97a73ec2edb2232eeb63e
giterasmus <h.erasmus@gmx.net>
2021-05-20 08:43:28 +0200

    Benennung angepasst, Fehler Spielstand behoben

--- main.ts
+++ main.ts
@@ -1,3 +1,38 @@
+/**
+ * Weiterentwicklung von DriverGame
+ */
+function FahrerTrainer () {
+    basic.clearScreen()
+    if (BegrenzungslichterPosition == 5) {
+        if (keinHindernis != position) {
+            zustand = 1
+        } else {
+            Spielstand += 1
+            erhöheV(Spielstand)
+        }
+        BegrenzungslichterPosition = 0
+        keinHindernis = randint(0, 4)
+        BegrenzungsLichter = []
+        for (let i = 0; i <= 4; i++) {
+            if (keinHindernis != i) {
+                BegrenzungsLichter.push(i)
+            }
+        }
+        return 0
+    }
+    for (let j = 0; j <= 3; j++) {
+        led.plot(BegrenzungsLichter[j], BegrenzungslichterPosition)
+    }
+    BegrenzungslichterPosition += 1
+    led.plot(position, 4)
+    return 0
+}
+function SpielEnde () {
+    basic.showIcon(IconNames.No)
+    basic.pause(1000)
+    basic.showNumber(Spielstand - 1)
+    basic.pause(200)
+}
 input.onButtonPressed(Button.A, function () {
     if (position == 0) {
         position = 1
@@ -6,44 +41,14 @@ input.onButtonPressed(Button.A, function () {
     position += -1
     led.plot(position, 4)
 })
-function DriverGame () {
-    basic.clearScreen()
-    if (linePosition == 5) {
-        if (emptySlot != position) {
-            state = 1
-        } else {
-            score += 1
-            erhöheV(score)
-        }
-        linePosition = 0
-        emptySlot = randint(0, 4)
-        lineLights = []
-        for (let i = 0; i <= 4; i++) {
-            if (emptySlot != i) {
-                lineLights.push(i)
-            }
-        }
-        return 0
-    }
-    for (let j = 0; j <= 3; j++) {
-        led.plot(lineLights[j], linePosition)
-    }
-    linePosition += 1
-    led.plot(position, 4)
-    return 0
-}
-function gameOver () {
-    basic.showIcon(IconNames.No)
-    basic.pause(1000)
-    basic.showNumber(score)
-}
 input.onButtonPressed(Button.AB, function () {
-    state = 0
-    score = 0
+    zustand = 0
+    Spielstand = 0
 })
-function erhöheV (myscore: number) {
-    if (score % 3 == 0) {
-        v = v * 0.8
+// Wenn der Spielstand durch 3 teilbar ist, dann erhöhe die Geschwindigkeit um 20%
+function erhöheV (SpSt: number) {
+    if (Spielstand % 3 == 0) {
+        Geschwindigkeit = Geschwindigkeit * 0.8
     }
 }
 input.onButtonPressed(Button.B, function () {
@@ -54,23 +59,23 @@ input.onButtonPressed(Button.B, function () {
     position += 1
     led.plot(position, 4)
 })
-let lineLights: number[] = []
-let emptySlot = 0
-let v = 0
-let linePosition = 0
+let BegrenzungsLichter: number[] = []
+let keinHindernis = 0
+let Geschwindigkeit = 0
+let BegrenzungslichterPosition = 0
 let position = 0
-let state = 0
-let score = 0
-score = 0
-state = 0
+let zustand = 0
+let Spielstand = 0
+Spielstand = 0
+zustand = 0
 position = 0
-linePosition = 5
-v = 1000
+BegrenzungslichterPosition = 5
+Geschwindigkeit = 1000
 basic.forever(function () {
-    if (state == 0) {
-        DriverGame()
-    } else if (state == 1) {
-        gameOver()
+    if (zustand == 0) {
+        FahrerTrainer()
+    } else if (zustand == 1) {
+        SpielEnde()
     }
-    basic.pause(v)
+    basic.pause(Geschwindigkeit)
 })
